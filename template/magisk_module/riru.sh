f9794cbe205d3c2b37a423bc723b1922589c26f7
RikkaW <rikka@shizuku.moe>
2021-03-11 22:55:26 +0800

    Use magisk --path

--- template/magisk_module/riru.sh
+++ template/magisk_module/riru.sh
@@ -12,17 +12,21 @@ RIRU_MODULE_API_VERSION=%%%RIRU_MODULE_API_VERSION%%%
 RIRU_MODULE_MIN_API_VERSION=%%%RIRU_MODULE_MIN_API_VERSION%%%
 RIRU_MODULE_MIN_RIRU_VERSION_NAME="%%%RIRU_MODULE_MIN_RIRU_VERSION_NAME%%%"
 
+if [ "$MAGISK_VER_CODE" -ge 21000 ]; then
+  MAGISK_CURRENT_RIRU_MODULE_PATH=$(magisk --path)/.magisk/modules/riru-core
+else
+  MAGISK_CURRENT_RIRU_MODULE_PATH=/sbin/.magisk/modules/riru-core
+fi
+
 # This function will be used when util_functions.sh not exists
 check_riru_version() {
-  RIRU_CORE_MODULES_PATH=/data/adb/modules/riru-core
-  RIRU_CORE_MODULES_UPDATE_PATH=/data/adb/modules_update/riru-core
-  if [ ! -f "$RIRU_CORE_MODULES_UPDATE_PATH/api_version" ] && [ ! -f "$RIRU_CORE_MODULES_PATH/api_version" ] && [ ! -f "/data/adb/riru/api_version" ] && [ ! -f "/data/adb/riru/api_version.new" ]; then
+  if [ ! -f "$MAGISK_CURRENT_RIRU_MODULE_PATH/api_version" ] && [ ! -f "/data/adb/riru/api_version" ] && [ ! -f "/data/adb/riru/api_version.new" ]; then
     ui_print "*********************************************************"
     ui_print "! Riru $RIRU_MODULE_MIN_RIRU_VERSION_NAME or above is required"
     ui_print "! Please install Riru from Magisk Manager or https://github.com/RikkaApps/Riru/releases"
     abort "*********************************************************"
   fi
-  RIRU_API=$(cat "$RIRU_CORE_MODULES_UPDATE_PATH/api_version") || RIRU_API=$(cat "$RIRU_CORE_MODULES_PATH/api_version") || RIRU_API=$(cat "/data/adb/riru/api_version.new") || RIRU_API=$(cat "/data/adb/riru/api_version") || RIRU_API=0
+  RIRU_API=$(cat "$MAGISK_CURRENT_RIRU_MODULE_PATH/api_version") || RIRU_API=$(cat "/data/adb/riru/api_version.new") || RIRU_API=$(cat "/data/adb/riru/api_version") || RIRU_API=0
   [ "$RIRU_API" -eq "$RIRU_API" ] || RIRU_API=0
   ui_print "- Riru API version: $RIRU_API"
   if [ "$RIRU_API" -lt $RIRU_MODULE_MIN_API_VERSION ]; then
@@ -46,20 +50,16 @@ enforce_install_from_magisk_app() {
   fi
 }
 
-if [ -f /data/adb/modules_update/riru-core/util_functions.sh ]; then
-  ui_print "- Load /data/adb/modules_update/riru-core/util_functions.sh"
-  . /data/adb/modules_update/riru-core/util_functions.sh
+if [ -f $MAGISK_CURRENT_RIRU_MODULE_PATH/util_functions.sh ]; then
+  ui_print "- Load $MAGISK_CURRENT_RIRU_MODULE_PATH/util_functions.sh"
+  # shellcheck disable=SC1090
+  . $MAGISK_CURRENT_RIRU_MODULE_PATH/util_functions.sh
 else
-  if [ -f /data/adb/modules/riru-core/util_functions.sh ]; then
-    ui_print "- Load /data/adb/modules/riru-core/util_functions.sh"
-    . /data/adb/modules/riru-core/util_functions.sh
+  if [ -f /data/adb/riru/util_functions.sh ]; then
+    ui_print "- Load /data/adb/riru/util_functions.sh"
+    . /data/adb/riru/util_functions.sh
   else
-    if [ -f /data/adb/riru/util_functions.sh ]; then
-      ui_print "- Load /data/adb/riru/util_functions.sh"
-      . /data/adb/riru/util_functions.sh
-    else
-      ui_print "- Can't find /data/adb/riru/util_functions.sh"
-    fi
+    ui_print "- Can't find /data/adb/riru/util_functions.sh"
   fi
 fi
 
